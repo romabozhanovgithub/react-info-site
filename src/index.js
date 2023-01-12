c7d23138f8b776f1cf5e1cf2c9e8e91f70da3f96
romabozhanovgithub <roma.bozhanov2017.11@gmail.com>
2023-01-12 19:29:02 +0300

    render list

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
 ReactDOM.render(
-    <h1>Hello, everyone!</h1>,
+    <ul>
+        <li>Thing 1</li>
+        <li>Thing 2</li>
+        <li>Thing 3</li>
+        <li>Thing 4</li>
+    </ul>,
     document.getElementById('root')
 );
