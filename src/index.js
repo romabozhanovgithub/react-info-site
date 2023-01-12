56b3ae2cccb1226ac010f070ef7a7674a85e4ac2
romabozhanovgithub <roma.bozhanov2017.11@gmail.com>
2023-01-12 19:51:02 +0300

    JSX

--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
-ReactDOM.render(
-    <h1 className="title">Hello World</h1>,
-    document.getElementById('root')
-);
-
 // const h1 = document.createElement('h1');
-// h1.textContent = 'Hello World';
+// h1.textContent = 'Hello World!';
 // h1.className = 'title';
 // document.getElementById('root').appendChild(h1);
+// console.log(h1);
+
+// JSX
+// const element = <h1 className="title">Hello World!</h1>;
+// console.log(element);
+// const page = (
+//     <div>
+//         <h1 className="header">This is JSX</h1>
+//         <p>This is a paragraph</p>
+//     </div>
+// )
+
+const navbar = (
+    <nav>
+        <h1>Title</h1>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+)
+
+ReactDOM.render(
+    navbar,
+    document.getElementById('root')
+);
